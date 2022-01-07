f264a560bd324b40c61a5edbc2d271b517d520b2
Abdullah Al Akash <aabdullahalakash@gmail.com>
2022-01-07 23:17:19 +0600

    Updated

--- src/Components/Booking/Booking/Booking.js
+++ src/Components/Booking/Booking/Booking.js
@@ -4,7 +4,7 @@ import { useParams } from 'react-router-dom/cjs/react-router-dom.min';
 const Booking = () => {
         const { bookingId } = useParams();
         return (
-                <div>
+                <div class="text-center">
                         <h3>Hello This is Booking {bookingId}</h3>
                 </div>
         );
