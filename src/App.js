f7d24ebb7f216aad2d23b582041926568aa246ea
Abdullah Al Akash <aabdullahalakash@gmail.com>
2022-01-09 23:51:25 +0600

    Updated

--- src/App.js
+++ src/App.js
@@ -10,29 +10,32 @@ import {
 import Booking from './Components/Booking/Booking/Booking';
 import Login from './Components/Login/Login';
 import Navbar from './Components/Navbar/Navbar';
+import AuthProvider from './Contexts/AuthProvider';
 
 function App() {
   return (
     <div className="">
-      <Router>
-        <Navbar />
-        <div className="mt-5 pt-3">
-          <Switch>
-            <Route exact path="/">
-              <Home />
-            </Route>
-            <Route path="/home">
-              <Home />
-            </Route>
-            <Route path="/booking/:bookingId">
-              <Booking />
-            </Route>
-            <Route path="/login">
-              <Login />
-            </Route>
-          </Switch>
-        </div>
-      </Router>
+      <AuthProvider>
+        <Router>
+          <Navbar />
+          <div className="mt-5 pt-3">
+            <Switch>
+              <Route exact path="/">
+                <Home />
+              </Route>
+              <Route path="/home">
+                <Home />
+              </Route>
+              <Route path="/booking/:bookingId">
+                <Booking />
+              </Route>
+              <Route path="/login">
+                <Login />
+              </Route>
+            </Switch>
+          </div>
+        </Router>
+      </AuthProvider>
     </div>
   );
 }
---
+++ src/Hooks/useFirebase.js
@@ -0,0 +1,46 @@
+import { getAuth, signInWithPopup, GoogleAuthProvider, signOut, onAuthStateChanged } from "firebase/auth";
+import { useEffect, useState } from "react";
+
+const useFirebase = () => {
+        const [user, setUser] = useState({});
+        const auth = getAuth();
+
+        const signInUsingGoogle = () => {
+                const googleProvider = new GoogleAuthProvider();
+
+                // Sign In With Popup:
+                signInWithPopup(auth, googleProvider)
+                        .then(result => {
+                                setUser(result.user);
+                        })
+        }
+
+        // Logout Function:
+        const logOut = () => {
+                signOut(auth)
+                        .then(() => {
+
+                        })
+        }
+
+        // Observe User State Change:
+        useEffect(() => {
+                const unsubscribe = onAuthStateChanged(auth, user => {
+                        if (user) {
+                                setUser(user);
+                        }
+                        else {
+                                setUser({});
+                        }
+                });
+                return () => unsubscribe;
+        }, []);
+
+        return {
+                user,
+                signInUsingGoogle,
+                logOut
+        }
+}
+
+export default useFirebase;
